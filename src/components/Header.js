3d1d900130fed73dddbb15f219277482742645e5
Sakibul_Munna <sakibul.munnaa@gmail.com>
2022-05-10 03:30:45 +0600

    Replaced Nav.Link to LinkContainer.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,40 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Header = () => {
   return (
     <header>
       <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="home">
-            <img
-              src={require("../assets/logo.png")}
-              alt="logo"
-              width="50px"
-              height="50px"
-            />
-          </Navbar.Brand>
+          <LinkContainer to="/home">
+            <Navbar.Brand>
+              <img
+                src={require("../assets/logo.png")}
+                alt="logo"
+                width="50px"
+                height="50px"
+              />
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="home">Home</Nav.Link>
-              <Nav.Link href="blogs">Blogs</Nav.Link>
+              <LinkContainer to="/home">
+                <Nav.Link href="home">Home</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/blogs">
+                <Nav.Link href="blogs">Blogs</Nav.Link>
+              </LinkContainer>
               <Nav.Link href="checkout">Checkout</Nav.Link>
             </Nav>
             <Nav>
-              <Nav.Link href="about">About</Nav.Link>
-              <Nav.Link href="login">Login</Nav.Link>
+              <LinkContainer to="/about">
+                <Nav.Link href="about">About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link href="login">Login</Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
